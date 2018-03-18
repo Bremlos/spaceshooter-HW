823161d7a667a78c2e91ecad84edf1748a2c6514
Bremlos <37507892+Bremlos@users.noreply.github.com>
2018-03-18 16:26:53 +0200

    Add files via upload

---
+++ Script/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform leftTopCorner;
+    public Transform RightBottomCorner;
+
+
+    [Range(1, 50)]
+    public float HorizontalSpeed = 40;
+    [Range(1, 50)]
+    public float VerticalSpeed = 40;
+
+    void Update()
+    {
+        movment();
+
+        if (transform.position.z < leftTopCorner.position.z)
+            transform.position = new Vector3(transform.position.x, transform.position.y, leftTopCorner.position.z);
+
+        if(transform.position.x > leftTopCorner.position.x)
+            transform.position = new Vector3(leftTopCorner.position.x, transform.position.y, transform.position.z);
+
+        if (transform.position.z > RightBottomCorner.position.z)
+            transform.position = new Vector3(transform.position.x, transform.position.y, RightBottomCorner.position.z);
+
+        if (transform.position.x < RightBottomCorner.position.x)
+            transform.position = new Vector3(RightBottomCorner.position.x, transform.position.y, transform.position.z);
+    }
+    void movment()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSpeed);
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * VerticalSpeed);
+    }
+}
---
+++ Script/PlayerMissile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMissile : MonoBehaviour {
+
+    [Range(100, 1000)]
+    public float speed = 100;
+
+     void Start()
+    {
+       transform.rotation = transform.localRotation;
+    }
+
+    void Update ()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+	}
+}
